8ebd6156c77f18cd0053ae6de378532dd8795e00
harish <harishswami62@gmail.com>
2020-07-22 22:22:23 +0530

    todo card

---
+++ js/dev/app.js
@@ -0,0 +1,17 @@
+let searchBox = document.querySelector("#search_input_box");
+let searchBtn = document.querySelector("#search_1");
+searchBtn.addEventListener('click', function () {
+    searchBox.style.visibility = 'visible';
+});
+
+
+let closeSearch = document.querySelector('#close_search');
+
+closeSearch.addEventListener('click', function () {
+    searchBox.style.visibility = 'hidden';
+})
+// document.getElementById("search-btn").onclick = function() {myFunction()};
+
+// function myFunction() {
+//   document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+// }

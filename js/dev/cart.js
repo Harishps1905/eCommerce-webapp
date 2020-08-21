dbaf6d9978066d058010b3222b5b7e3abf23d503
harish <harishswami62@gmail.com>
2020-08-21 20:19:50 +0530

    updated cart

---
+++ js/dev/cart.js
@@ -0,0 +1,6 @@
+// let addCart = document.querySelector("#add-cart");
+
+// addCart.addEventListener('click', function () {
+//     let mainRow = document.querySelector(".main-row");
+//     mainRow.style.display = 'block';
+// });
